94a8a72cd3ddaee3831a066a279e421ef14b157d
Tom <tomarcher88@outlook.com>
2020-05-12 15:08:17 +0100

    Added comments to JS and minor layout change to HTML - Added Stop Sign

--- Anonymask/censor.js
+++ Anonymask/censor.js
@@ -1,95 +1,68 @@
+// Selectors
 const video = document.querySelector('.webcam');
 const videoCanvas = document.querySelector('.video');
 const videoCtx = videoCanvas.getContext('2d');
 const faceCanvas = document.querySelector('.face');
 const faceCtx = faceCanvas.getContext('2d');
-let faceCovering = video;
+let faceCovering = null;
 
+// Selects each emoji as an object so that the drawImage function can use it
+// If you just select the src it's stored as a string and can't be used in drawImage
 const emojiList = document.querySelectorAll('.emoji');
 function changeEmoji(e) {
-  faceCovering = e.path[0];
+  if (e.path[0].alt !== 'Stop Sign Emoji') {
+    faceCovering = e.path[0];
+  } else {
+    faceCovering = null;
+  }
 }
 emojiList.forEach(emoji => emoji.addEventListener('click', changeEmoji));
 
+// Creates an instance of the FaceDetector
 const faceDetector = new window.FaceDetector();
 // console.log(video, videoCanvas, faceCanvas, faceDetector);
 
+// Grabs the webcam footage and puts it onto the video and canvas elements
+// async/await used to allow for the time it takes for the webcam to actually kick in
 async function populateVideo() {
   const stream = await navigator.mediaDevices.getUserMedia({
     video: { width: 1280, height: 720 },
   });
   video.srcObject = stream;
   await video.play();
-  // console.log(video.videoWidth, video.videoHeight);
   videoCanvas.width = video.videoWidth;
   videoCanvas.height = video.videoHeight;
   faceCanvas.width = video.videoWidth;
   faceCanvas.height = video.videoHeight;
 }
 
+// Awaits a face to enter the video to detect
 async function detect() {
   const faces = await faceDetector.detect(video);
   faces.forEach(censor);
-  faces.forEach(drawFace);
   requestAnimationFrame(detect);
 }
 
-function drawFace(face) {
-  // destructoring the boundingBox
-  const { width, height, top, left } = face.boundingBox;
-  // Clearing each box before rendering another, to prevent multiple drawings on screen
-  videoCtx.clearRect(0, 0, videoCanvas.width, videoCanvas.height);
-  // Styling the lines of the rendered box
-  videoCtx.strokeStyle = '#ffc60000';
-  videoCtx.lineWidth = 2;
-  // Drawing the box over the face
-  videoCtx.strokeRect(left, top, width, height);
-}
 
+// Puts the emoji over the face
 function censor({ boundingBox: face }) {
-  // Prevent image smoothing, which helps with the pixelated look we're goin for
-  faceCtx.imageSmoothingEnabled = false;
+  // The below clears the previous frames emoji, without this the emoji would be printed to the canvas and stay there
+  // Meaning you'd end up with just a mess of stacked emojis on the screen
   faceCtx.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
-  // The below takes a snapshot of the detected face
-  // Saves it as a small copy, ready to be resized larger to pixelate
-  if (faceCovering === video) {
+  // If faceCovering isn't null it'll put the emoji onto the face, otherwise it'll not put anything on the face
+  if (faceCovering !== null) {
     faceCtx.drawImage(
-      // 5 Source arguments
-      video, // Where does the source come from
-      face.x, // What location of the source video do we start at
-      face.y,
-      face.width,
-      face.height,
-      // 4 draw arguments
-      face.x, // Where do we start to DRAW the X & Y?
-      face.y,
-      8,
-      8
-    );
-    faceCtx.drawImage(
-      faceCanvas,
-      face.x,
-      face.y,
-      8,
-      8,
-      face.x - 75, // H - Where to start
-      face.y - 75, // V - Where to start
-      face.width + 150, // How big
-      face.height + 150 // How big
-    );
-  } else {
-    faceCtx.drawImage(
-      faceCovering, // Where does the source come from
+      faceCovering, // Where does the source come from, this has to be an HTML element and not a string
+      0, // These two zeros represent where in the source image would you like to start drawing from, which 0 0 means top left corner (to get the whole img)
       0,
-      0,
+      512, // The two 512s are how far into the image would you like to draw to. The emojis are 512 x 512 and I want the whole emoji drawn.
       512,
-      512,
-      face.x - 75, // H - Where to start
-      face.y - 75, // V - Where to start
-      face.width + 150, // How big
-      face.height + 150 // How big
+      face.x - 75, // H - Where to put the top corner of the image, I've used negative values here to allow for the image being a bit bigger than the box around the face
+      face.y - 75, // V 
+      face.width + 150, // This is how big should the image be, I've chosen to go larger than the face to completely cover it
+      face.height + 150 // 
     );
-  }
+  } 
 }
 
 populateVideo().then(detect);
