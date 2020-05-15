e2ab28c653a2815bf1cbc828ef4717403545b30c
Tom <tomarcher88@outlook.com>
2020-05-15 12:50:39 +0100

    First upload

--- 100DaysOfCode/script.js
+++ 100DaysOfCode/script.js
@@ -1,8 +1,23 @@
+// Countdown variables
 // When the challenge ends
 const endDate = new Date("2020-08-22T12:30:00").getTime();
 // Grab the HTML element we want the timer to be displayed in
 const countdownHTML = document.querySelector('#countdown');
 const progressBar = document.querySelector('.progressBar');
+
+// Fade out variables
+const fadeMe = document.querySelectorAll('.fade-me'); // .style.opacity
+const fadeTracker = document.querySelector('.fade-tracker');
+// Fadeout function
+function scrollHandler() {
+  // Grabs 
+  fadeAmount = fadeTracker.getBoundingClientRect().bottom;
+  console.log('They see me scrolling')
+  fadeMe.forEach(el => el.style.opacity = `${(fadeAmount - 250) * 4 / 10}%`);
+
+}
+window.addEventListener('scroll', scrollHandler);
+
 
 // Luckily this is a 100 day challenge, so just grabbed the code from above and modified it slightly to give a `percentage`, which I'll use for graphic on main page
 function countdownPercentage(deadline) {
@@ -12,6 +27,8 @@ function countdownPercentage(deadline) {
   const percent = 100 - days;
   return progressBar.setAttribute('value', percent);
 }
+
+
 
 function calculateTimeRemaining(deadline) {
   // Get current time
