7c547ceb8ddda129c104ba7e59737e989e7173f4
Tom <tomarcher88@outlook.com>
2020-06-10 10:08:16 +0100

    Updated comments and fixed bugs

---
+++ Weather/location-data.js
@@ -0,0 +1,62 @@
+export const locationData = [
+  {
+    "id": 2643743,
+    "name": "London",
+    "state": "",
+    "country": "GB",
+    "coord": {
+      "lon": -0.12574,
+      "lat": 51.50853
+    }
+  },
+  {
+    "id": 2643123,
+    "name": "Manchester",
+    "state": "",
+    "country": "GB",
+    "coord": {
+      "lon": -2.23743,
+      "lat": 53.480949
+    }
+  },
+  {
+    "id": 2655603,
+    "name": "Birmingham",
+    "state": "",
+    "country": "GB",
+    "coord": {
+      "lon": -1.89983,
+      "lat": 52.481419
+    }
+  },
+  {
+    "id": 3333167,
+    "name": "Liverpool",
+    "state": "",
+    "country": "GB",
+    "coord": {
+      "lon": -2.91667,
+      "lat": 53.416672
+    }
+  },
+  {
+    "id": 2654710,
+    "name": "Brighton",
+    "state": "",
+    "country": "GB",
+    "coord": {
+      "lon": -0.13947,
+      "lat": 50.828381
+    }
+  },
+  {
+    "id": 2654675,
+    "name": "Bristol",
+    "state": "",
+    "country": "GB",
+    "coord": {
+      "lon": -2.59665,
+      "lat": 51.455231
+    }
+  },
+]
